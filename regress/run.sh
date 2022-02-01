98e20b3662d9f53dc971dafec1fa933e0fa55a19
Dennis Felsing <dfelsing@yugabyte.com>
2022-02-01 17:21:56 +0000

    Fix runner

--- regress/run.sh
+++ regress/run.sh
@@ -4,6 +4,6 @@ for i in **/*.sql; do
   bin/psql -c "drop database postgis_reg;"
   rm -r /tmp/pgis_reg
   PATH=/nfusr/dev-server/dfelsing/code/postgis/regress/bin/:$PATH ./run_test.pl $i
-  cp /tmp/pgis_reg/test1_diff $i.diff
+  cp /tmp/pgis_reg/test_1_diff $i.diff
   sleep 10
 done &> test.out
